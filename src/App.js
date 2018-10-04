3575668803b3f583c0f99cec967408130c727b25
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-10-04 13:38:22 -0400

    appSetup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import chat_bot from "./component/chat_bot.js";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return <h1>test</h1>;
+    return (
+      <div className="App">
+        <div className="header">
+          <h1>This is sepBot here to show you more about Sepehr</h1>
+        </div>
+        <div className="theme">
+          <chat_bot />
+        </div>
+      </div>
+    );
   }
 }
 
--- src/component/chat_bot.js
+++ src/component/chat_bot.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import theme from "./theme.js";
-import Steps from "Steps.js";
+import Steps from "./Steps.js";
 
 class chat_bot extends Component {
   render() {
     return (
       <div>
-        <theme steps={steps} />
+        <theme Steps={Steps} />
       </div>
     );
   }
