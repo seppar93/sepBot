2e803f7238b7b84b3997dc88c656a7d00c23ac7a
Sepehr Parirokh <sepehrparirokh@gmail.com>
2018-10-04 13:21:39 -0400

    project3

---
+++ src/component/project3.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Crud = () => {
+  return (
+    <div>
+      <a href="https://onedot-web-app.herokuapp.com/login?redirect=%2F">
+        React CRUD app
+      </a>
+    </div>
+  );
+};
+
+export default Crud;
